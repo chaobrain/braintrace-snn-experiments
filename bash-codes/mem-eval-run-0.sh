00ae1892c7f2c2c1cf4a93257d56bc76500d1459
Chaoming Wang <adaduo@outlook.com>
2024-09-13 15:44:55 +0800

    remove bash codes

--- bash-codes/mem-eval-run-0.sh
+++
@@ -1,5 +0,0 @@
-for i in 50 100 200 300 400 600 800 1000
-do
-python task-memory-and-speed-evaluation.py --devices 0 --data_length $i --epoch 1 --n_layer 3 --n_rec 512 --method bptt --model lif-delta --exp_name mem-opt-v2 --warmup_ratio 0.
-done
-
--- bash-codes/mem-eval-run-1.sh
+++
@@ -1,5 +0,0 @@
-for i in 50 100 200 300 400 600 800 1000
-do
-python task-memory-and-speed-evaluation.py --devices 3 --data_length $i --epoch 1 --n_layer 3 --n_rec 512 --method expsm_diag --etrace_decay 0.9 --model lif-delta --memory_eval 1 --drop_last 1 --exp_name mem-opt-v2  --warmup_ratio 0.
-done
-
